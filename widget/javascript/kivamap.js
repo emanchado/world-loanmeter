48e5da75b76983a040d633103c23a57954851abc
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2009-05-21 13:29:51 +0200

    Collect loan data per sector too
    * Refactor the code a bit so the loan HTML is only calculated once, then
      used for both loans by location and loans by sector
    * Change the 'loan-location' CSS class to 'loan-group-title'
    * The widget, at this point, has references to Javascript functions that
      don't exist yet

--- widget/javascript/kivamap.js
+++ widget/javascript/kivamap.js
@@ -1,19 +1,31 @@
 function KivaMap (mapSelector, mapCssSelector, data) {
-    this.mapSelector        = mapSelector;
-    this.mapCssSelector     = mapCssSelector;
-    this.data               = data;
-    this.htmlChunksForPlace = {};
+    this.mapSelector         = mapSelector;
+    this.mapCssSelector      = mapCssSelector;
+    this.data                = data;
+    this.htmlChunksForPlace  = {};
+    this.htmlChunksForSector = {};
 
     this.placeLoans = function () {
         var self = this;
         this.data.eachLoan(function () {
           var loan = this;
+          var loanHtml = self.data.loanInfoWithTemplate(loan,
+                          '<div class="loan-info">' +
+                            '<a href="LOANLINK"><img src="LOANSMALLIMGURL" ' +
+                            'alt="LOANTOWN, LOANCOUNTRY" /></a>' +
+                            '<div class="loan-title">' +
+                              '<a href="LOANLINK">BORROWERNAME</a></div>' +
+                              '<div class="loanuse">LOANUSE</div>' +
+                              '<div class="loanstatus">LOANSTATUS</div>' +
+                              '<div class="loansectoractivity"><a href="#" onclick="map.showSectorInfoInPanel(\'LOANSECTOR\')">LOANSECTOR</a> - ' +
+                                'LOANACTIVITY</div>' +
+                            '</div>');
 
           // Collect all the loans in each place, and put only one dot in the
           // map for each place
           var placeId = 'location' +
                             loan.location.country.replace(/[^a-z]/gi, '-');
-          if (self.htmlChunksForPlace[placeId] == undefined) { // First loan
+          if (self.htmlChunksForPlace[placeId] == undefined) {
             [lat, lon] = loan.location.geo.pairs.split(' ');
 
             mapMaker.placeIdByLatitudeAndLongitude(self.mapCssSelector,
@@ -28,23 +40,26 @@ function KivaMap (mapSelector, mapCssSelector, data) {
             var f = function () { map.showLocationInfoInPanel(placeId); return false };
             $('#'+placeId).mouseover(f).focus(f);
 
-            self.htmlChunksForPlace[placeId] = ['<div class="loan-location">' +
-                                                  loan.location.country +
-                                                  '</div>'];
+            self.htmlChunksForPlace[placeId] =
+                ['<div class="loan-group-title">' +
+                    loan.location.country +
+                    ' (<a href="#" ' +
+                       'onclick="map.showSectorDirectoryInPanel()">Browse ' +
+                       'Sectors</a>)</div>'];
           }
 
-          self.htmlChunksForPlace[placeId].push(
-                  self.data.loanInfoWithTemplate(loan,
-                      '<div class="loan-info">' +
-                        '<a href="LOANLINK"><img src="LOANSMALLIMGURL" ' +
-                        'alt="LOANTOWN, LOANCOUNTRY" /></a>' +
-                        '<div class="loan-title">' +
-                          '<a href="LOANLINK">BORROWERNAME</a></div>' +
-                          '<div class="loanuse">LOANUSE</div>' +
-                          '<div class="loanstatus">LOANSTATUS</div>' +
-                          '<div class="loansectoractivity">LOANSECTOR - ' +
-                            'LOANACTIVITY</div>' +
-                        '</div>'));
+          self.htmlChunksForPlace[placeId].push(loanHtml);
+
+          var sectorId = loan.sector;
+          if (self.htmlChunksForSector[sectorId] == undefined) {
+              self.htmlChunksForSector[sectorId] =
+                  ['<div class="loan-group-title">' + loan.sector +
+                    ' (<a href="#" ' +
+                        'onclick="map.showSectorDirectoryInPanel()">' +
+                        'All Sectors</a>)</div>'];
+          }
+
+          self.htmlChunksForSector[sectorId].push(loanHtml);
 
           // If any of the loans needs more money, add the 'needmoney' class
           if (parseFloat(loan.loan_amount) -
